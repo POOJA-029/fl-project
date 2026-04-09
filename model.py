a0e4379fb6d3f1f3f9083091766020577093bf85
Pooja Malusare <poojamalusare29@gmail.com>
2026-04-09 13:01:18 +0530

    fixed model error

--- model.py
+++ model.py
@@ -1,17 +1,2 @@
 def create_model():
     return None
-
-def create_model():
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(16, activation='relu', input_shape=(8,)),
-        tf.keras.layers.Dense(8, activation='relu'),
-        tf.keras.layers.Dense(1, activation='sigmoid')
-    ])
-
-    model.compile(
-        optimizer='adam',
-        loss='binary_crossentropy',
-        metrics=['accuracy']
-    )
-
-    return model
